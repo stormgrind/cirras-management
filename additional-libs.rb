eac972332f4025a6e5c1d4774d885f229d9ad92f
Marek Goldmann <marek.goldmann@gmail.com>
2009-07-17 10:20:48 +0200

    moved back to gems

--- additional-libs.rb
+++ additional-libs.rb
@@ -1,4 +1,6 @@
-additional_libs = [ "eventmachine", "sinatra", "amazon-ec2", "thin", "rack", "rest-client" ]
+additional_libs = [ "sinatra", "amazon-ec2", "rest-client" ]
+
+# "thin", "rack", "eventmachine"
 
 additional_libs.each do |lib|
  $LOAD_PATH.unshift( "#{File.dirname( __FILE__ )}/lib/#{lib}/lib" )
