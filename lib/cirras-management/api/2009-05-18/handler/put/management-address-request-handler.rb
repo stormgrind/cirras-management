3c370621a0cffd073bea0e9e65f0749a74eb1759
Marek Goldmann <marek.goldmann@gmail.com>
2009-11-30 11:26:26 +0100

    injection of PEER_ID to /etc/jboss-as5/conf file

--- lib/cirras-management/api/2009-05-18/handler/put/management-address-request-handler.rb
+++ lib/cirras-management/api/2009-05-18/handler/put/management-address-request-handler.rb
@@ -55,6 +55,14 @@ module CirrASManagement
         when APPLIANCE_TYPE[:backend]
           # if we're a back-end appliance, ask for front-end appliance address to inject it to /etc/jboss-as5.conf
 
+          unless File.exists?( @jboss_as5_conf_file )
+            @log.error "File #{@jboss_as5_conf_file} does not exists! Starting JBoss AS failed."
+            return
+          end
+
+          jboss_as5_conf = File.read( @jboss_as5_conf_file )
+          jboss_as5_conf_file_changed = false
+
           @log.info "Asking for front-end appliance address..."
 
           front_end_addresses = @client_helper.get( "http://#{address}:#{@config.port}/latest/address/#{APPLIANCE_TYPE[:frontend]}" )
@@ -67,6 +75,8 @@ module CirrASManagement
           if front_end_addresses.size == 0
             @log.info "No front-end appliances running, skipping."
 
+            # pgrep -f "/usr/lib/jvm/jre/bin/java (.*) -classpath /opt/jboss-as5/bin/run.jar"
+
             Thread.new do
               jboss_stop
             end
@@ -74,53 +84,79 @@ module CirrASManagement
             return
           end
 
-          front_end_address = front_end_addresses.first
+          if @peer_id.nil?
+            peer_id = @client_helper.get( "http://#{address}:#{@config.port}/latest/peer-id" )
 
-          if @front_end_address != front_end_address
-            @front_end_address = front_end_address
-
-            @log.info "Injecting front-and appliance address #{@front_end_address} to '#{jboss_as5_conf_file}'..."
-            # inject front-end appliance address to /etc/jboss-as5.conf
-
-            unless File.exists?( @jboss_as5_conf_file )
-              @log.error "File #{@jboss_as5_conf_file} does not exists! Injecting front-end appliance address failed."
+            if peer_id.to_i != 0
+              @peer_id = peer_id
+            else
+              @log.error "Received PEER_ID = #{peer_id} is not valid."
               return
             end
 
-            jboss_as5_conf  = File.read( @jboss_as5_conf_file )
+            @log.info "Injecting PEER_ID = #{@peer_id} to '#{@jboss_as5_conf_file}'..."
 
-            pattern_proxy   = /^JBOSS_PROXY_LIST=(.*)/
-            pattern_gossip  = /^JBOSS_GOSSIP_HOST=(.*)/
+            pattern_peer_id = /^JBOSS_SERVER_PEER_ID=(.*)/
+            matches_peer_id = jboss_as5_conf.match( pattern_peer_id )
+            directive_peer_id = "JBOSS_SERVER_PEER_ID=#{@peer_id}"
 
-            matches_proxy   = jboss_as5_conf.match( pattern_proxy )
-            matches_gossip  = jboss_as5_conf.match( pattern_gossip )
+            if matches_peer_id.nil?
+              # no JBOSS_PROXY_LIST line, adding one
+              jboss_as5_conf += "\n\n#{directive_peer_id}"
+            else
+              jboss_as5_conf.gsub!( pattern_peer_id, directive_peer_id )
+            end
 
-            directive_proxy   = "JBOSS_PROXY_LIST=#{@front_end_address}:80"
-            directive_gossip  = "JBOSS_GOSSIP_HOST=#{@front_end_address}"
+            `sudo sh -c "echo '#{jboss_as5_conf}' > #{@jboss_as5_conf_file}"`
+
+            jboss_as5_conf_file_changed = true
+
+            @log.info "PEER_ID = #{@peer_id} injected."
+          end
+
+          if @front_end_addresses != front_end_addresses
+            @front_end_addresses = front_end_addresses
+
+            @log.info "Injecting front-and appliance address #{@front_end_addresses} to '#{@jboss_as5_conf_file}'..."
+            # inject front-end appliance address to /etc/jboss-as5.conf
+
+            pattern_proxy = /^JBOSS_PROXY_LIST=(.*)/
+            pattern_gossip = /^JBOSS_GOSSIP_HOST=(.*)/
+
+            matches_proxy = jboss_as5_conf.match( pattern_proxy )
+            matches_gossip = jboss_as5_conf.match( pattern_gossip )
+
+            # TODO: what with multiple gossip_hosts?
+            directive_proxy = "JBOSS_PROXY_LIST=#{@front_end_addresses.join(":80,")}:80"
+            directive_gossip = "JBOSS_GOSSIP_HOST=#{@front_end_addresses.first}"
 
             if matches_proxy.nil?
               # no JBOSS_PROXY_LIST line, adding one
               jboss_as5_conf += "\n\n#{directive_proxy}"
             else
-              jboss_as5_conf.gsub!( pattern_proxy, directive_proxy  )
+              jboss_as5_conf.gsub!( pattern_proxy, directive_proxy )
             end
 
             if matches_gossip.nil?
               # no JBOSS_GOSSIP_HOST line, adding one
               jboss_as5_conf += "\n\n#{directive_gossip}"
             else
-              jboss_as5_conf.gsub!( pattern_gossip, directive_gossip  )
+              jboss_as5_conf.gsub!( pattern_gossip, directive_gossip )
             end
 
             `sudo sh -c "echo '#{jboss_as5_conf}' > #{@jboss_as5_conf_file}"`
 
-            Thread.new do
-              jboss_stop
-              jboss_start
-            end
-          end
-        else
+            jboss_as5_conf_file_changed = true
 
+            @log.info "Front-end addresses injected."
+          end
+
+          Thread.new do
+            jboss_stop
+            jboss_start
+          end if jboss_as5_conf_file_changed
+
+        else
       end
 
     end
