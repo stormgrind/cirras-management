5c127d25af8aee5274da18aea4555aaf9bc6443c
Marek Goldmann <marek.goldmann@gmail.com>
2010-05-27 10:04:17 +0200

    removing gems, using now rubygems

--- additional-libs.rb
+++
@@ -1,8 +0,0 @@
-additional_libs = [ "sinatra", "amazon-ec2", "rest-client" ]
-
-# "thin", "rack", "eventmachine"
-
-additional_libs.each do |lib|
- $LOAD_PATH.unshift( "#{File.dirname( __FILE__ )}/lib/#{lib}/lib" )
-end
-
